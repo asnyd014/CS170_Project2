085a098bc4c1674deae6e0f89f9f238dbcc1d258
asnyd014 <91281848+asnyd014@users.noreply.github.com>
2024-05-16 21:46:37 -0700

    Initial implementation of Forward Selection Algorithm

--- main.cpp
+++ main.cpp
@@ -5,8 +5,14 @@ https://stackoverflow.com/questions/2704521/generate-random-double-numbers-in-c
 #include <iostream>
 #include <random>
 #include <ctime>
+#include <algorithm>
 
 using namespace std;
+
+struct FeatureNode {
+    vector<int> features;
+    double accuracy;
+};
 
 void forwardSelection(int totalFeatures);
 void backwardElimination(int totalFeatures);
@@ -25,7 +31,56 @@ int main() {
 }
 
 void forwardSelection(int totalFeatures) {
+    // Create list of feature values to be used for creation of node later
+    vector<int> featureValueList;
+    for (int i = 0; i < totalFeatures; ++i) {
+        featureValueList.push_back(i+1);
+    }
 
+    // Create initial empty feature node
+    FeatureNode initialNode;
+    initialNode.accuracy = evaluation();
+
+    // Nodes representing the tentative next choice and current maximum 
+    FeatureNode nextNode = initialNode;
+    FeatureNode maxNode = initialNode;
+
+    vector<FeatureNode> nodeOptions;
+
+    // For the number of total features, perform Forward Selection algorithm
+    for (int i = 0; i < totalFeatures; ++i) {
+
+        // Create combinations using features in the nextNode and featureValueList
+        for (int j = 0; j < featureValueList.size(); ++j) {
+            if (find(nextNode.features.begin(), nextNode.features.end(), featureValueList[j]) != nextNode.features.end()) { // Feature is not already in the nextNode's features
+                FeatureNode newNode;
+                vector<int> newFeatures = nextNode.features;
+                newFeatures.push_back(j);
+                newNode.features = newFeatures;
+                newNode.accuracy = evaluation();
+                nodeOptions.push_back(newNode);
+            
+                /*
+                 * "Using features" output goes here, use information from newNode
+                 */
+            }
+        }
+
+        // Choose the next node from nodeOptions, update maxNode if a higher accuracy node is found
+        nextNode = nodeOptions[0];
+        for (int j = 0; j < nodeOptions.size(); ++j) {
+            if (nodeOptions[j].accuracy > nextNode.accuracy) { nextNode = nodeOptions[j]; } // Choose new next node
+            if (nodeOptions[j].accuracy > maxNode.accuracy) { maxNode = nodeOptions[j]; } // Update max node
+        }
+
+        /*
+         * "Feature set was best" output goes here, use information from nextNode
+         */
+    }
+
+    /*
+     * "Finished search" output goes here, use information from maxNode
+     */ 
 }
 
 void backwardElimination(int totalFeatures) {

a973b09ccac9c628497852eb7b43035dea96a0b5
asnyd014 <91281848+asnyd014@users.noreply.github.com>
2024-05-24 21:41:55 -0700

    Created initial version of test driver

--- Classifier.h
+++ Classifier.h
@@ -1,4 +1,5 @@
 #include <vector>
+#include <cmath>
 
 class Classifier {
     vector<vector<float>> trainingInstances;
@@ -9,6 +10,27 @@ class Classifier {
     }
 
     float test(int testID) {
+        float minDistance = INT_MAX;
+        int minIndex = 0;
+        float tempDistance = 0;
         // Calculate Euclidean distance to all trainingInstances to determine class
+        for (int i = 0; i < trainingInstances.size(); ++i) {
+            if (i != testID) { // Avoid checking distance to itself
+                tempDistance = 0;
+                for (int j = 1; j < trainingInstances[0].size(); ++j) { // Iterate through provided features only, index will be the same between instances
+                    tempDistance += pow((trainingInstances[i][j] - trainingInstances[testID][j]), 2);
+                }
+                tempDistance = sqrt(tempDistance);
+                
+                // Update minimum if applicable
+                if (tempDistance < minDistance) {
+                    minDistance = tempDistance;
+                    minIndex = i;
+                }
+            }
+        }
+
+        // Return class of instance at minIndex
+        return trainingInstances[minIndex][0];
     }
 }
---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include "Validator.h"
+
+int main() {
+    vector<vector<float>> trainingData;
+    vector<int> featureSubset({3, 5, 7});
+    // Read data from file into trainingData
+
+    Classifer NN;
+    Validator v(NN);
+    v.leaveOneOutValidation(featureSubset, trainingData);
+
+    return 0;
+}
